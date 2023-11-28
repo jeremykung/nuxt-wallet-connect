5c60ccc98b7aba5fe493ad1a564bf6f84fc80dd5
es <es@ess-MacBook-Air.local>
2023-11-29 00:06:04 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <w3m-button />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5/vue";
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = "2ddac9090b846a6d08a48a50e1299e7a";
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: "Ethereum",
+  currency: "ETH",
+  explorerUrl: "https://etherscan.io",
+  rpcUrl: "https://cloudflare-eth.com",
+};
+
+// 3. Create modal
+const metadata = {
+  name: "My Website",
+  description: "My Website description",
+  url: "https://mywebsite.com",
+  icons: ["https://avatars.mywebsite.com/"],
+};
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains: [mainnet],
+  projectId,
+});
+</script>
